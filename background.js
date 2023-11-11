834e38781b5231b88d25a103164698a168ac82bb
Niels van Dam <nielsvdam@hotmail.com>
2023-11-11 01:31:13 +0100

    update to use newer neopets version

--- background.js
+++ background.js
@@ -1,23 +1,41 @@
 //Your safety deposit box
 if (window.location.pathname === '/safetydeposit.phtml') {
-  console.log('Scanning Safety Box...');
-  var list = [];
-  var items = $('tr[bgcolor] td[align=left]:nth-child(2) b');
-  filterData(items, list);
-  findPrice(list, 'sdb')
-}
+  var search_button_location = $('.submit_data').parent();
+  $(`<input type="button" id="search_prices" style="margin-right: 8px" value="Search JellyNeo Prices">`).insertBefore('.submit_data');
 
-//For viewing gallery items
-if (window.location.pathname === '/gallery/index.phtml') {
-  console.log('Scanning Gallery...');
-  var list = [];
-  var items = $('b.textcolor');
-  //filterData(items, list);
-  findPrice(list, 'gallery');
+  var onCooldown = false;
+  $('#search_prices').click(function () {
+    if (onCooldown) {
+      if ($('#please_wait').length > 0) return;
+
+      $('<b style="margin-right: 8px" id="please_wait">Wait 1 min...</b>').insertBefore('#search_prices');
+
+      setTimeout(function () {
+        $('#please_wait').remove();
+      }, 3000);
+      return;
+    }
+
+    $('<b style="margin-right: 8px" id="loading_search_text">Searching...</b>').insertBefore('#search_prices');
+
+    console.log('Scanning Safety Box...');
+    $('b[id=item_jellyneo_np]').remove();
+
+    var list = [];
+    var items = $('tr[bgcolor] td[align=left]:nth-child(2) b');
+
+    filterData(items, list);
+    findPrice(list, 'sdb')
+
+    onCooldown = true;
+    setTimeout(function () {
+      onCooldown = false;
+    }, 60000);
+  });
 }
 
 //For user shop
-if(window.location.pathname === '/market.phtml'){
+if (window.location.pathname === '/market.phtml') {
   console.log('Scanning shop items..');
   var list = []
   var items = $('td.content form table tbody  td:nth-child(1) b');
@@ -25,68 +43,68 @@ if(window.location.pathname === '/market.phtml'){
   findPrice(list, 'shop');
 }
 
-//On view of item in the inventory
-if(window.location.pathname ==='/iteminfo.phtml'){
-  console.log('Scanning item');
-  var list = []
-  var items = $('body table:nth-child(2) tbody tr td:nth-child(2)').html().trim().split('<br>')[0].split(':')[1].trim();
-  var arr = [];
-  arr.push(items);
-  findPrice(arr, 'item');
-}
-
 //User's inventory
-if(window.location.pathname ==='/inventory.phtml'){
-  console.log('Scanning inventory items..');
-  var list = []
-  var items = $('td.content div:nth-child(14) table tbody tr:nth-child(2) td table');
-  filterData(items, list);
-  list = list[0].trim().split('\n');
-  $.each(list, function(index,obj){
-    //remove if null
-    if(obj != null){
-      if(obj == ''){
-        list.splice(index, 1);
+if (window.location.pathname === '/inventory.phtml') {
+  var filterListElement = $('.inv-filtericons');
+
+  filterListElement.append(`<div style="width:20%" class="wizard-button-search button-default__2020 button-blue__2020" id="search_prices"><div class="button-search-white" style="background-image: url('https://images.neopets.com/themes/h5/basic/images/np-icon.svg');
+  height: 22px;
+  width: 100%;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;"></div></div>`);
+
+  var onCooldown = true;
+  setTimeout(function () {
+    onCooldown = false;
+    $('#search_prices').click(function () {
+      if (onCooldown) {
+        if ($('#please_wait').length > 0) return;
+
+        filterListElement.append('<b style="margin-left: 8px" id="please_wait">Wait 1 min...</b>');
+        setTimeout(function () {
+          $('#please_wait').remove();
+        }, 3000);
+        return;
       }
-    }
-  })
-  findPrice(list, 'inventory');
+      filterListElement.append('<b style="margin-left: 8px" id="loading_search_text">Searching...</b>');
+      console.log('Scanning inventory items..');
+
+      $('b[id=item_jellyneo_np]').remove();
+
+      var list = []
+      var items = $('.inv-container .inv-items .inv-display .item-grid .grid-item .item-name');
+      filterData(items, list);
+      findPrice(list, 'inventory');
+      onCooldown = true;
+
+      setTimeout(function () {
+        onCooldown = false;
+      }, 60000);
+    });
+
+  }, 2000);
 }
 
 //Shops
-if(window.location.pathname ==='/objects.phtml'){
+if (window.location.pathname === '/objects.phtml') {
   console.log('Scanning inventory items..');
   var list = []
-  var items = $('body table:nth-child(2) tbody tr td:nth-child(2)').html().trim().split('<br>')[0].split(':')[1];
+  var items = $('.shop-grid .shop-item .item-name b');
   filterData(items, list);
   findPrice(list, 'shops');
-}
-
-//Money tree
-if(window.location.pathname ==='/donations.phtml'){
-  console.log('Scanning donations items..');
-  var list = []
-  var items = $('.name')
-  $.each(items, function(item){
-    var textContent = items[item].textContent
-    console.log(textContent)
-    list.push(textContent)
-  })
-
-  console.log(list)
-  findPrice(list, 'donations');
 }
 
 var highestPrice = 0
 var itemToGet = ''
 //Ajac request to jellyneo's database to fetch the prices
-function findPrice(list, path){
+function findPrice(list, path) {
 
-  $.each(list, function(a, string) {
-    if(string != 'Enter your PIN:' && string!= 'PIN' && string !='Name'){
+  $.each(list, function (a, string) {
+    if (string != 'Enter your PIN:' && string != 'PIN' && string != 'Name') {
       return $.ajax({
-        url: "http://items.jellyneo.net/index.php?go=show_items&name=" + string + "&name_type=exact&desc=&cat=0&specialcat=0&status=0&rarity=0&sortby=name&numitems=1",
-        success: function(res){
+        url: "https://items.jellyneo.net/index.php?go=show_items&name=" + string + "&name_type=exact&desc=&cat=0&specialcat=0&status=0&rarity=0&sortby=name&numitems=1",
+        success: function (res) {
           var price;
           console.info("Trying ... " + string);
           price = +$(res).find('.price-history-link').text().replace(RegExp(" NP"), '').replace(RegExp(","), '');
@@ -94,42 +112,29 @@ function findPrice(list, path){
 
           //Looping through the table again as the order is lost during ajax call
 
-          if(path =='shop'){
-            $.each($('td.content form table tr td:nth-child(1) b'), function(row){
-              if($('td.content form table tr td:nth-child(1) b').eq(a).html()==string){
-                $('td.content form table tbody td:nth-child(7) input').eq(a-1).val(price)
-              }
-            })
-          }else if(path =='item'){
-            $('body table:nth-child(6) tbody').append('<td><b>JellyNeo Price:<b></td><td>' + price + ' NP </td>');
-          }else if(path =='inventory'){
-
-            $.each($('td.content div:nth-child(14) table tbody tr:nth-child(2) td table td'), function(row){
-              if($('td.content div:nth-child(14) table tbody tr:nth-child(2) td table td').eq(a).html().split('<br>')[1]== string){
-                if($('td.content div:nth-child(14) table tbody tr:nth-child(2) td table td').eq(a).hasClass("price") == false)
-                $('td.content div:nth-child(14) table tbody tr:nth-child(2) td table td').eq(a).append('<div class="price"><br><b>' + price + ' NP </b></div>')
-              }
-            })
-          } else if(path =='donations'){
-            $('p:contains("' + string +'")').html( ' ' + price + ' NP </td>');
-            if (price > highestPrice){
-              highestPrice = price
-              itemToGet = string
-            }
-          }else if(path == 'sdb'){
-            $('b:contains("' + string +'")').append( ' ' + price + ' NP </td>');
+          if (path == 'shops') {
+            $('b:contains("' + string + '")').parent().parent().append('<p> <b style="color: #d15517; font-size: 0.8rem"> ' + price + ' NP</b></p>');
+          } else if (path == 'inventory') {
+            $('#loading_search_text').remove();
+            $('p:contains("' + string + '")').parent().append('<p id="item_jellyneo_np"> <b style="color: #d15517; font-size: 0.8rem"> ' + price + ' NP</b></p>');
+          } else if (path == 'sdb') {
+            $('#loading_search_text').remove();
+            $('b:contains("' + string + '")').parent().append('<b id="item_jellyneo_np" style="color: #d15517"><br> ' + price + ' NP</b> </td>');
           }
+        },
+        error: function (err) {
+          console.log(`something went wrong while looking for item ${string}`);
         }
       })
     }
   });
 }
 
-function filterData(items, list){
-  $.each(items, function(a, b) {
+function filterData(items, list) {
+  $.each(items, function (a, b) {
     var item, root;
     root = $(this);
-    item = root.text().replace(/\(.*?\)+/, '');
+    item = root.text().replace(/ *\([^)]*\) */g, "");
     if (item) {
       return list.push(item);
     }
